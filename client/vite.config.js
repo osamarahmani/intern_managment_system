1fec7db27782749df93380ea90dc95f04046cfcf
Valiant-Vishal <vishal300406@gmail.com>
2026-06-10 16:57:55 +0530

    perf: split xlsx into separate chunk and increase chunk size warning limit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('xlsx')) {
+              return 'xlsx';
+            }
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'vendor';
+            }
+          }
+        }
+      }
+    }
+  }
 })
